cc4f79a3eef84c88a68578ac9a67ff02152873c6
Xinwei Xie Vivian <xx2185@nyu.edu>
2021-11-12 15:31:35 +0800

    update

--- my-real-first web/js/script.js
+++ my-real-first web/js/script.js
@@ -28,3 +28,42 @@ let ee
 function say(ee){
   let name=ee
 }
+
+function setup (){
+  createCanvas(500,500);
+  rectMode(CENTER);
+
+  background(0,0,0)
+}
+
+function draw(){
+
+  translate(width/2, height/2);
+  stroke(255,255,255);
+  strokeWeight(5)
+  noFill();
+
+
+  let angle = sin(frameCount/200)*5;
+  let angle2 = sin(frameCount/300)*50;
+  let radius=400
+
+  let r = map(cos(angle), -1, 1, 0, 255);
+  let g = map(sin(angle), -1, 1, 0, 255);
+  let b = map(noise(angle), 0, 1, 0, 255);
+  let posX = radius * cos(angle) * noise(angle);
+  let posY = radius * sin(angle) * noise(angle);
+  push()
+  background(0,0,0)
+  rotate(angle);
+  stroke(r, g, b, 200);
+  ellipse(0,0,posX,posY)
+  rect(0,0,posX,posY)
+  ellipse(0,0,posY,posX)
+  rect(0,0,posY,posX)
+  fill(r,g,b,100)
+  ellipse(0,0,sin(angle/100)*1000)
+  pop()
+  angle=angle+1
+
+}
