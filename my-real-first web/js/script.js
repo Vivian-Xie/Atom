f1865b97a293b0df9cb3c60bfd8e5fe0bda70206
Xinwei Xie Vivian <xx2185@nyu.edu>
2021-11-12 15:02:38 +0800

    First Websute

---
+++ my-real-first web/js/script.js
@@ -0,0 +1,30 @@
+// let roungbutton= document.getElementById('roundButton');
+// roundButton.addEventListener('click',roundButtonClicked);
+// function roundButtonClicked(){
+//   let rectElem=document.getElementById('rect');
+//   rectElem.style.borderRadius='50px'
+// }
+// // alert("Hello")
+//
+//
+// function say(what){
+//   let elem = document.getElementById('lyrics');
+//
+//   // append
+//   //document.body.appendChild(newElement);
+//   let newElem = document.createElement('p');
+//   newElem.innerHTML = what;
+//
+//   elem.appendChild(newElem);
+//
+//   elem=document.getElementById('heading');
+//   if (what=='great'){
+//     elem.style.backgroundColor='red'
+//   }
+//
+// }
+// // elem.style.backgroundColor='rgb(124,12,12)'
+let ee
+function say(ee){
+  let name=ee
+}
